6c772fd7408a9401d763b9e8a677966e73b151fa
Silas Gyger <silasgyge@gmail.com>
2023-06-12 15:51:11 +0200

    remove superfluous period

--- conf.py
+++ conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Audiotool'
-copyright = '2023, Audiotool Inc.'
-author = 'Audiotool Inc.'
+copyright = '2023, Audiotool Inc'
+author = 'Audiotool Inc'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
