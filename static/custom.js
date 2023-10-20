b7c2999acf07077a2e200bf54a3ef0763dbb3bf1
Silas Gyger <silasgyge@gmail.com>
2023-10-20 10:02:57 +0200

    enable plausible through js

--- static/custom.js
+++ static/custom.js
@@ -4,4 +4,14 @@ window.onload = () => {
         document.querySelector(".wy-nav-side").setAttribute("style", "display: none");
         document.querySelector(".wy-nav-content-wrap").setAttribute("style", "margin-left: 0px !important");
     }
+};
+
+// enable plausible
+{
+    const scriptElement = new DOMParser()
+        .parseFromString(
+            '<script defer data-domain="manual.audiotool.com" src="https://www.audiotool.com/js/script.js"></script>',
+            'text/html'
+        ).querySelector("script");
+    document.querySelector("head").append(scriptElement);
 }
