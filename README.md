76398af18bb4b1988bf8fd15e5078614fa6fc8e2
jujurocket <julien.melion@audiotool.com>
2025-07-30 13:25:36 +0100

    add cookie banner

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This is the repository of the official audiotool manual - read at [manual.audiotool.com](https://manual.audiotool.com).
 
 Head over to [How to Contribute](https://manual.audiotool.com/manuals/other/contribute.html) if you'd like to contribute!
+
+## How to run
+
+- [Install Sphinx](https://www.sphinx-doc.org/en/master/usage/installation.html)
+- Install a Sphinx extension:
+```shell
+python3 -m pip install sphinx-rtd-theme sphinx-sitemap
+```
+- Re-generate the static page with:
+```shell
+make html
+```
--- static/custom.js
+++ static/custom.js
@@ -6,12 +6,23 @@ window.onload = () => {
     }
 };
 
-// enable plausible
 {
-    const scriptElement = new DOMParser()
+    // enable plausible
+    const plausibleScript = new DOMParser()
         .parseFromString(
             '<script defer data-domain="manual.audiotool.com" src="https://www.audiotool.com/js/script.js"></script>',
             'text/html'
         ).querySelector("script");
-    document.querySelector("head").append(scriptElement);
+    document.querySelector("head").append(plausibleScript);
+
+    // cookie banner
+    const cookieScript = document.createElement('script');
+    cookieScript.type = 'module';
+    cookieScript.textContent = `
+        console.log("cookie banner");
+        import { banner } from "https://at-cdn-s04.audiotool.com/javascript/cookie-banner.js";
+        const settingsDialogue = banner({googleTagId: "G-JBQGMZED6F"});
+        settingsDialogue.show();
+    `;
+    document.querySelector("head").prepend(cookieScript);
 }
